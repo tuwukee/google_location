c52d1c78e2a9424fd03574942d945f0d3cf855d4
Yulia Oletskaya <voletsky@gmail.com>
2013-05-23 14:34:03 +0300

    new rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleApp::Application.config.secret_token = 'e7ec4b32d13a452751be7775fbeb87d34b06c468d9e869eadaf42ba6eee770f5e19654a4c7dde26fdd6be660d33de78f5fdf686b73b7e2716606693c708fac4c'
